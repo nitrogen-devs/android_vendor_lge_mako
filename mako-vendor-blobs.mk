1a5c9541248c45f5fb1066b6741bd68bbd197abd
Valeriy <binder_z@mail.ru>
2016-01-03 17:16:48 +0400

    Fixes, fixes, fixes!!! Prepared for Nitrogen OS
    I very much swearing, causing everything in order.
    Only a blithering idiot could make such proprietary blobs.
    
    >_< WTF!?!?!?!

---
+++ mako-vendor-blobs.mk
@@ -0,0 +1,9 @@
+# =========================================
+# mako-vendor-blobs.mk mako
+# This file is created by Mr.MEX
+# =========================================
+
+LOCAL_PATH := vendor/lge/mako/proprietary/system
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/*:system/*
