0f8bead202bb1beefc75a5ca26ef2cd65f465442
Valeriy <binder_z@mail.ru>
2016-01-06 13:15:35 +0400

    Deleted!

--- mako-vendor-blobs.mk
+++
@@ -1,9 +0,0 @@
-# =========================================
-# mako-vendor-blobs.mk mako
-# This file is created by Mr.MEX
-# =========================================
-
-LOCAL_PATH := vendor/lge/mako/proprietary/system
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/*:system/*
